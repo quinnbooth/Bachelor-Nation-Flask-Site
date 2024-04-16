af8e060e26a52daeafa0120b50461674ccb9dc41
David Nguyen <david.nguyen@columbia.edu>
2024-04-16 18:53:28 -0400

    Added mult select and fill in blank

--- main/static/quiz.js
+++ main/static/quiz.js
@@ -9,6 +9,10 @@ $("#document").ready(function () {
     multChoiceHandler(data.choices, data.answer[0]);
   } else if (data.questionType === "true_false") {
     trueFalseHandler(data.answer[0]);
+  } else if (data.questionType === "fill_blank") {
+    fillBlankHandler(data.answer[0]);
+  } else if (data.questionType === "mult_select") {
+    multSelectHandler(data.choices, data.answer);
   } else {
     console.log(data.questionType);
   }
@@ -84,6 +88,84 @@ function trueFalseHandler(answer) {
   $("#quizSubmit").on("click", function () {
     let id = current_choice.attr("id");
     if (id === answer.toString()) {
+      submitHandler({
+        isCorrect: true,
+        id: data.questionId,
+      });
+    } else {
+      submitHandler({
+        isCorrect: false,
+        id: data.questionId,
+      });
+    }
+  });
+}
+
+function fillBlankHandler(answer) {
+  $("#questionBody").append(
+    `
+      <div id="fillBlankContainer"></div>
+    `
+  );
+  $("#fillBlankContainer").append(
+    `
+      <input class="form-control" id="fillAnswer" placeholder="Type here">
+    `
+  );
+
+  $("#quizSubmit").on("click", function () {
+    let guess = $("#fillAnswer").val();
+    if (guess.toLowerCase() === answer.toString().toLowerCase()) {
+      submitHandler({
+        isCorrect: true,
+        id: data.questionId,
+      });
+    } else {
+      submitHandler({
+        isCorrect: false,
+        id: data.questionId,
+      });
+    }
+  });
+}
+
+function multSelectHandler(choices, answer) {
+  $("#questionBody").append(
+    `
+      <div id="multChoiceContainer"></div>
+    `
+  );
+  for (let i = 0; i < choices.length; i++) {
+    $("#multChoiceContainer").append(
+      `
+        <input type="checkbox" id="choice${i}" class="btn-check mult-select">
+        <label class="btn btn-outline-primary" for="choice${i}">${choices[i]}</label> 
+      `
+    );
+  }
+  $(".mult-select").hover(function () {
+    $(this).css("background-color", "gray");
+  });
+  $(".mult-select").mouseleave(function () {
+    $(this).css("background-color", "lightgray");
+  });
+
+  $("#quizSubmit").on("click", function () {
+    let guesses = [];
+
+    for (let i = 0; i < choices.length; i++) {
+      if ($(`#choice${i}`).prop("checked")) {
+        guesses.push(1);
+      } else {
+        guesses.push(0);
+      }
+    }
+
+    for (let i = 0; i < answer.length; i++) {
+      guesses[answer[i]]--;
+    }
+
+    if (guesses.every((element) => element === 0)) {
       submitHandler({
         isCorrect: true,
         id: data.questionId,
