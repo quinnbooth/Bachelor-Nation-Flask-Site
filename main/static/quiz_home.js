920ac6cee637acc629486fec10bd4e0cfca31f75
David Nguyen <david.nguyen@columbia.edu>
2024-04-16 21:13:24 -0400

    Added final quiz score

--- main/static/quiz_home.js
+++ main/static/quiz_home.js
@@ -1,11 +1,16 @@
 $("#document").ready(function () {
-  for (let i = 1; i < 7; i++) {
-    $("#quizContainer").append(
+  $("#quizContainer").append(
+    `
+        <div id="quizQuestions"></div>
       `
-                <div class="questionBox" id="question${i}">
-                    Question ${i}
-                </div>
-            `
+  );
+  for (let i = 1; i < 7; i++) {
+    $("#quizQuestions").append(
+      `
+        <div class="questionBox" id="question${i}">
+            Question ${i}
+        </div>
+      `
     );
     if (data.correct.includes(i.toString())) {
       $(`#question${i}`).addClass("correct");
@@ -24,4 +29,16 @@ $("#document").ready(function () {
   $(".unanswered").mouseleave(function () {
     $(this).css("background-color", "#f9cb9c");
   });
+
+  if (data.correct.length + data.incorrect.length === 6) {
+    $("#quizContainer").append(
+      `
+        <div>
+          Total Score: ${data.correct.length}/6 (${Math.round(
+        (data.correct.length * 100) / 6
+      )}%)
+        </div>
+      `
+    );
+  }
 });
